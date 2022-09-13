c3065bcb74dc671107d184dac67ba072ed5b0307
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-09-13 20:45:46 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 6516141723 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/Piron-Hub-NEW-VERSION/main/DOORSscript.lua"))()
+end
+
+if game.PlaceId == 7560156054 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/Piron-Hub-NEW-VERSION/main/Clicker%20Simulator%20script.lua"))()
+end
