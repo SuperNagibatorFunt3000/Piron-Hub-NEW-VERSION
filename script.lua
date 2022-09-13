3dea0d324f20f07515ec806488707a3b03de293f
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-09-13 21:03:27 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -9,3 +9,7 @@ end
 if game.PlaceId == 9534337535 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/Piron-Hub-NEW-VERSION/main/%F0%9F%A7%BD%20Shrek%20in%20the%20Backrooms%20(New%20Levels!).lua"))()
 end
+
+if game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/Piron-Hub-NEW-VERSION/main/Prison%20Life%20script.lua"))()
+end
